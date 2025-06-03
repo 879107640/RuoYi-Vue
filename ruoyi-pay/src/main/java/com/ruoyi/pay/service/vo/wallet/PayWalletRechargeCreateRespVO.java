d59b681e5763812da92ffb80a2c0d3daf045e37d
lzzh <1782994419@qq.com>
2025-06-03 18:33:31 +0800

    refactor(ruoyi-pay): 优化代码结构和可读性
    - 为 AfterSaleConvert 类添加文档注释，说明其功能和使用方法
    - 移除 AfterSaleStatusEnum 中未使用的状态码
    - 在 PayWalletRechargeCreateRespVO 中为长整型字段添加 @JsonSerialize 注解，确保正确序列化

--- ruoyi-pay/src/main/java/com/ruoyi/pay/service/vo/wallet/PayWalletRechargeCreateRespVO.java
+++ ruoyi-pay/src/main/java/com/ruoyi/pay/service/vo/wallet/PayWalletRechargeCreateRespVO.java
@@ -1,5 +1,7 @@
 package com.ruoyi.pay.service.vo.wallet;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
@@ -8,9 +10,11 @@ import lombok.Data;
 public class PayWalletRechargeCreateRespVO {
 
   @Schema(description = "钱包充值编号", requiredMode = Schema.RequiredMode.REQUIRED, example = "1")
+  @JsonSerialize(using = ToStringSerializer.class)
   private Long id;
 
   @Schema(description = "支付订单编号", requiredMode = Schema.RequiredMode.REQUIRED, example = "100")
+  @JsonSerialize(using = ToStringSerializer.class)
   private Long payOrderId;
 
 }
